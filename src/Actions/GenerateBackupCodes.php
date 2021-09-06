6e102e4f463e030a2cc9b1b87902a7ce43511650
Meletis Flevarakis <meletisf@users.noreply.github.com>
2021-09-06 18:50:56 +0000

    Apply fixes from StyleCI

--- src/Actions/GenerateBackupCodes.php
+++ src/Actions/GenerateBackupCodes.php
@@ -10,8 +10,7 @@ class GenerateBackupCodes
     {
         $codes = [];
 
-        for ($i = 0; $i < $total; $i++)
-        {
+        for ($i = 0; $i < $total; $i++) {
             $codes[] = implode('-', [Str::random(5), Str::random(5)]);
         }
 
