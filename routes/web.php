6852138e70ad9701434ecb7bcaa7cc63a52b1219
meletisf <fmeletis85@gmail.com>
2021-09-08 21:20:36 +0300

    Added throttle middleware

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use MakelarisJR\Laravel2FA\Http\Controllers\Verify2FAController;
 
-Route::view('/verify', 'laravel2fa::verify')
-    ->name('verify2fa');
-Route::post('/verify', Verify2FAController::class)
-    ->name('verify2fa.post');
+Route::group(['middleware' => 'throttle:100'], function() {
+    Route::view('/verify', 'laravel2fa::verify')
+        ->name('verify2fa');
+    Route::post('/verify', Verify2FAController::class)
+        ->name('verify2fa.post')->middleware('throttle:10');
+});
