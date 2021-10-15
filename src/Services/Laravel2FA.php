72731fae09e3c2fbf32f2115d53995c585023de7
makelarisjr <8687447+makelarisjr@users.noreply.github.com>
2021-10-15 16:16:17 +0300

    Initialize Yubikey service only when Yubikey is enabled in the config file

--- src/Services/Laravel2FA.php
+++ src/Services/Laravel2FA.php
@@ -2,6 +2,8 @@
 
 namespace MakelarisJR\Laravel2FA\Services;
 
+use MakelarisJR\Laravel2FA\Exception\GoogleOTPDisabledException;
+use MakelarisJR\Laravel2FA\Exception\YubikeyOTPDisabledException;
 use PragmaRX\Google2FA\Google2FA;
 
 class Laravel2FA
@@ -12,11 +14,15 @@ class Laravel2FA
     public function __construct()
     {
         $this->google2fa  = new Google2FA();
-        $this->yubikey2fa = new Yubikey(
-            config('laravel2fa.yubikey.client_id'),
-            config('laravel2fa.yubikey.secret_key'),
-            config('laravel2fa.yubikey.api_server')
-        );
+
+        if (config('laravel2fa.yubikey.enabled'))
+        {
+            $this->yubikey2fa = new Yubikey(
+                config('laravel2fa.yubikey.client_id'),
+                config('laravel2fa.yubikey.secret_key'),
+                config('laravel2fa.yubikey.api_server')
+            );
+        }
     }
 
     /**
@@ -34,9 +40,15 @@ class Laravel2FA
      * @throws \PragmaRX\Google2FA\Exceptions\IncompatibleWithGoogleAuthenticatorException
      * @throws \PragmaRX\Google2FA\Exceptions\InvalidCharactersException
      * @throws \PragmaRX\Google2FA\Exceptions\SecretKeyTooShortException
+     * @throws \MakelarisJR\Laravel2FA\Exception\GoogleOTPDisabledException
      */
     public function verifyGoogleOTP(string $otp, string $secret)
     {
+        if (!config('laravel2fa.google.enabled'))
+        {
+            throw new GoogleOTPDisabledException('Google OTP is disabled.');
+        }
+
         return $this->google2fa->verifyKey(
             $secret,
             $otp,
@@ -44,8 +56,16 @@ class Laravel2FA
         );
     }
 
+    /**
+     * @throws \MakelarisJR\Laravel2FA\Exception\YubikeyOTPDisabledException
+     */
     public function verifyYubikeyOTP(string $otp): bool
     {
+        if (!config('laravel2fa.yubikey.enabled'))
+        {
+            throw new YubikeyOTPDisabledException('Yubikey OTP is disabled.');
+        }
+
         return $this->yubikey2fa->verifyOtp($otp);
     }
 }
