fcfa8bc2bdfcbe6c39d7f1fe18592014f241789d
meletisf <fmeletis85@gmail.com>
2021-11-01 22:48:27 +0200

    added ShouldVerify Middleware

---
+++ src/Http/Middleware/ShouldVerify.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace MakelarisJR\Laravel2FA\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Session;
+
+class ShouldVerify
+{
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @return \Illuminate\Http\RedirectResponse|mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = $request->user();
+
+        if (! $user->has2FAEnabled() or Session::has('2fa_passed'))
+        {
+            return redirect(
+                config('laravel2fa.default_redirect', '/dashboard')
+            );
+        }
+
+        return $next($request);
+    }
+}
